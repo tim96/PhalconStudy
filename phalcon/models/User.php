e8fdc51b90655d1ae23ea68c9e2d386c62b26ff0
Alexander <tim96@yandex.ru>
2016-05-21 08:59:21 +0400

    Add 'User' model.

---
+++ phalcon/models/User.php
@@ -0,0 +1,65 @@
+<?php
+
+use Phalcon\Mvc\Model;
+
+class User extends Model
+{
+    private $id;
+
+    private $name;
+
+    private $email;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
